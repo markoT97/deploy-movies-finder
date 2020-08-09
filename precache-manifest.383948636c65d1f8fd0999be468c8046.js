2ed0bbe84b9a8a519cfcd94b9625740122531ca6
Marko Tričković <marko.trickovic13@gmail.com>
2020-08-10 01:21:32 +0200

    Updates

---
+++ precache-manifest.383948636c65d1f8fd0999be468c8046.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b689926e87a6fca91976e8af09cd56",
+    "url": "/deploy-movies-finder/index.html"
+  },
+  {
+    "revision": "7a95c4b74b8318b00baf",
+    "url": "/deploy-movies-finder/static/css/main.2c463cf5.chunk.css"
+  },
+  {
+    "revision": "092c44bf041b75aa9ac4",
+    "url": "/deploy-movies-finder/static/js/2.d0f52887.chunk.js"
+  },
+  {
+    "revision": "dc2e56807efd38a08b058e1e18ec3be8",
+    "url": "/deploy-movies-finder/static/js/2.d0f52887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a95c4b74b8318b00baf",
+    "url": "/deploy-movies-finder/static/js/main.053c76dd.chunk.js"
+  },
+  {
+    "revision": "46de8f8a39fc1505beee",
+    "url": "/deploy-movies-finder/static/js/runtime-main.5b5f4ee7.js"
+  },
+  {
+    "revision": "40c511360e91add1cdce4d159c2d8027",
+    "url": "/deploy-movies-finder/static/media/imdb-logo.40c51136.svg"
+  },
+  {
+    "revision": "e7d69c9bb33ccdfbdb90350d1ebe8a75",
+    "url": "/deploy-movies-finder/static/media/no-poster-available.e7d69c9b.jpg"
+  }
+]);
